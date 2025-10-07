ac5d0edc9fa1598884ec8802888432e26023eea8
Hazrat90 <127823732+Hazrat90@users.noreply.github.com>
2025-10-07 12:25:04 -0700

    Create next.config.js

---
+++ talent-protocol-builder-rewards/next.config.js
@@ -0,0 +1,25 @@
+
+
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+reactStrictMode: true,
+}
+module.exports = nextConfig
+```
+
+
+---
+
+
+## postcss.config.js
+
+
+```js
+module.exports = {
+plugins: {
+tailwindcss: {},
+autoprefixer: {},
+},
+}
+```
